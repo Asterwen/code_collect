dadb1206780b442592ccd878579619163b7d9a1f
aster <2012078441@qq.com>
2019-08-07 10:15:29 +0800

    初始化

---
+++ Code/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="testFunc">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  created () {
+    
+    
+    var bar = function() {
+            return 'world';
+        };
+    console.log(typeof foo); // function pointer    
+    // function foo() {
+    //     return 'hello';
+    // }
+    var foo = 'hello'
+    console.log(typeof bar); // undefined
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
